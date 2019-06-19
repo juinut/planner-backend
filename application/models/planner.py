dd0fc76f370f76b2c734507317229aac361fe4a6
PhantomKey <punstack@hotmail.com>
2019-06-19 15:53:04 +0700

    add Planner tabel

--- application/models/planner.py
+++ application/models/planner.py
@@ -4,11 +4,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from ._base import db
 
 
-class User(db.Model):
+class Planner(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255), unique=True)
+    first_date = db.Column(db.DateTime)
     email = db.Column(db.String(255), unique=True)
-    firstname = db.Column(db.String(255),nullable=False)
+    firstname = db.Column(db.String(255), nullable=False)
     lastname =  db.Column(db.String(255), nullable=False)
     # avatar = db.Column(db.String(200), default='default.png')
     password = db.Column(db.String(255))
