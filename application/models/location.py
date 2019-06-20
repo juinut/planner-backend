a6adff0da2a6d8664efe07a6c846a9c4c07124f1
PhantomKey <punstack@hotmail.com>
2019-06-20 11:44:43 +0700

    finish db

--- application/models/location.py
+++ application/models/location.py
@@ -5,8 +5,8 @@ from ._base import db
 class Location(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
-    latitude = db.Column(db.Float(Precision=24), nullable=False)
-    longtitude = db.Column(db.Float(Precision=24), nullable=False)
+    latitude = db.Column(db.Float(precision=24), nullable=False)
+    longtitude = db.Column(db.Float(precision=24), nullable=False)
     #ForeignKey
     # relationship
     start = db.relationship("Service", backref='start_loc', lazy='dynamic')
