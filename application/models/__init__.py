5cb0d1019e611ec9fb08fd3ca05f1e88ff69b1be
PhantomKey <punstack@hotmail.com>
2019-06-20 11:40:01 +0700

    fix db import

--- application/models/__init__.py
+++ application/models/__init__.py
@@ -1,3 +1,12 @@
 from ._base import db
 from .user import *
 from .role import *
+from .activity import *
+from .joinMemberPlannerActivity import *
+from .location import *
+from .member import *
+from .planner import *
+from .role import *
+from .service import *
+from .serviceType import *
+
