e642990bb836adfae0a1a6e4e545b2717df5dec3
PhantomKey <punstack@hotmail.com>
2019-06-20 17:53:27 +0700

    add simple user login api

--- application/controllers/api_v1/users.py
+++ application/controllers/api_v1/users.py
@@ -23,17 +23,21 @@ def get_user(user_id):
     else:
         abort(400)
 
-# @bp.route('/login' methods=['POST'])
-# def get_token(user):
-#     try:
-#         user_name = request.json.get('username')
-#         password = request.json.get('password')
-#         if not user_name:
-#             raise Exception('username is empty')
-#         else if not password:
-#             raise Exception('password is empty')
-#         user = User.query.get(user_name)
-#         if user:
-#             if user.password == password:
-#                 jwt_auth.
-#         log = json.loads(user)
+@bp.route('/login' methods=['POST'])
+def get_token(user):
+    try:
+        token = False
+        user_name = request.json.get('username')
+        password = request.json.get('password')
+        if not user_name:
+            raise Exception('username is empty')
+        else if not password:
+            raise Exception('password is empty')
+        user = User.query.get(user_name)
+        if user:
+            if user.password == password:
+                token = True
+            return jsonif(dict(success=token), code=200)
+    except Exception as e:
+        return jsonify(dict(message=str(e)), code=404)
+                
